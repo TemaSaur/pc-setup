dd35796c2af85fe6a3ecfc5bd9d77f4e8dfc25c8
Artemy Vakhrushev <69428015+TemaSaur@users.noreply.github.com>
2024-05-30 11:16:41 +0000

    add install commands

---
+++ installs.cmd
@@ -0,0 +1,100 @@
+# browsers
+winget install -e --id Google.Chrome
+winget install -e --id Mozilla.Firefox.DeveloperEdition
+winget install -e --id Brave.Brave
+
+
+# utils
+winget install -e --id File-New-Project.EarTrumpet
+winget install -e --id WinDirStat.WinDirStat
+winget install -e --id CrystalRich.LockHunter
+winget install -e --id StefanSundin.Superf4
+winget install -e --id Rainmeter.Rainmeter
+winget install -e --id Nvidia.GeForceExperience
+winget install -e --id Unchecky.Unchecky
+winget install -e --id Microsoft.PowerToys
+winget install -e --id JohnMacFarlane.Pandoc
+
+
+# file formats
+winget install -e --id 7zip.7zip
+winget install -e --id VideoLAN.VLC
+winget install -e --id nomacs.nomacs
+
+
+# other
+winget install -e --id Microsoft.WindowsTerminal
+winget install -e --id Starship.Starship
+winget install -e --id Chocolatey.Chocolatey
+
+
+# apps
+winget install -e --id OBSProject.OBSStudio
+winget install -e --id Figma.Figma
+winget install -e --id Figma.FigmaAgent
+winget install -e --id Inkscape.Inkscape
+winget install -e --id qBittorrent.qBittorrent
+winget install -e --id Notion.Notion
+winget install -e --id TheDocumentFoundation.LibreOffice
+iwr -useb https://raw.githubusercontent.com/spicetify/cli/main/install.ps1 | iex
+
+
+# socials
+winget install -e --id Discord.Discord
+winget install -e --id Microsoft.Teams
+winget install -e --id Zoom.Zoom
+
+
+# coding
+winget install -e --id Microsoft.VisualStudioCode
+winget install -e --id Neovim.Neovim
+winget install -e --id SublimeHQ.SublimeText.4
+winget install -e --id JetBrains.Toolbox
+winget install -e --id Git.Git
+winget install -e --id GitHub.GitHubDesktop
+winget install -e --id PostgreSQL.PostgreSQL
+winget install -e --id PostgreSQL.pgAdmin
+winget install -e --id Postman.Postman
+winget install -e --id Docker.DockerDesktop
+
+
+# gaming
+winget install -e --id Valve.Steam
+winget install -e --id EpicGames.EpicGamesLauncher
+winget install -e --id ElectronicArts.EADesktop
+
+
+# tools
+winget install -e --id OpenJS.NodeJS.LTS
+
+winget install -e --id Python.Python.3.9
+winget install -e --id Python.Python.3.12
+
+winget install -e --id Oracle.JDK.17
+winget install -e --id Oracle.JavaRuntimeEnvironment
+
+winget install -e --id Microsoft.DotNet.SDK.6
+winget install -e --id Microsoft.DotNet.SDK.7
+winget install -e --id Microsoft.DotNet.SDK.8
+winget install -e --id Microsoft.DotNet.Framework.DeveloperPack_4 -v 4.7.2
+
+winget install -e --id Microsoft.VCRedist.2005.x86
+winget install -e --id Microsoft.VCRedist.2005.x64
+winget install -e --id Microsoft.VCRedist.2008.x86
+winget install -e --id Microsoft.VCRedist.2008.x64
+winget install -e --id Microsoft.VCRedist.2010.x86
+winget install -e --id Microsoft.VCRedist.2010.x64
+winget install -e --id Microsoft.VCRedist.2012.x86
+winget install -e --id Microsoft.VCRedist.2012.x64
+winget install -e --id Microsoft.VCRedist.2013.x86
+winget install -e --id Microsoft.VCRedist.2013.x64
+winget install -e --id Microsoft.VCRedist.2015+.x86
+winget install -e --id Microsoft.VCRedist.2015+.x64
+
+winget install -e --id LLVM.LLVM
+
+
+# packages
+pip install thefuck ptpython uvicorn gunicorn
+npm i -g pnpm five-serve
+choco install pypy3 -y
